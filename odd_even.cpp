53cc44d20d88dda974af7b924f43e429172a6a7a
Anshul <72655193+AnsSec@users.noreply.github.com>
2022-10-25 13:19:34 +0530

    Create odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int n;
+ cout<<"Enter A Number To check weather is even or odd : ";
+ cin>>n;
+ if (n%2==0){
+    cout<<"This is even Number "<<endl;
+ } else{
+    cout<<"This is odd Number"<<endl;
+ }
+
+}
