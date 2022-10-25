b305b6742a5d6ed4f3666ad389e2683df280bd72
Anshul <72655193+AnsSec@users.noreply.github.com>
2022-10-25 21:00:45 +0530

    Create 3_Basic_Data_Types.cpp

---
+++ Hacker_Rank/3_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(10)<<d<<endl;//fix decimal value
+    cout<<fixed<<setprecision(20)<<e<<endl;
+
+    return 0;
+}
